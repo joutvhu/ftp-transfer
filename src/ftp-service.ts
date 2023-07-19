557bd21729c036aeda8557b0caa3cef95aab4013
Giao Ho <joutvhu@gmail.com>
2023-07-20 03:17:23 +0700

    Update

--- src/ftp-service.ts
+++ src/ftp-service.ts
@@ -1,7 +1,7 @@
-import * as core from '@actions/core';
+// import * as core from '@actions/core';
 import * as fs from 'fs';
 import Client from 'ftp';
-import {basename, dirname, join} from 'path';
+import {join} from 'path';
 import {isBlank, isNotBlank} from './io-helper';
 
 declare type Command = (service: FtpService) => Promise<any>;
@@ -89,7 +89,7 @@ export class FtpService {
             }
             throw new Error(`Unsupported command "${command}"`);
         });
-        core.info('Executing commands');
+        console.log('Executing commands');
         let result = {
             succeed: 0,
             message: null
@@ -103,18 +103,18 @@ export class FtpService {
             if (throwing)
                 throw e;
             result.message = e.message;
-            core.warning(e.message);
+            console.warn(e.message);
         }
         return result;
     }
 
     async list() {
         return execute(callback => {
-            core.info('Listing');
+            console.log('Listing');
             this.client.list((error, listing) => {
                 if (listing) {
                     for (const element of listing) {
-                        core.info(JSON.stringify(element));
+                        console.log(JSON.stringify(element));
                     }
                 }
                 callback(error);
@@ -122,76 +122,148 @@ export class FtpService {
         });
     }
 
-    async get(path: string, dest?: string) {
-        let br = 0, work = '.', next = '', over = '';
-        if (dest != null) {
-            if (dest.endsWith('/')) {
-                work = dest.slice(0, dest.length - 1);
-            } else {
-                work = dirname(dest);
-                over = basename(dest);
-            }
-        }
-        const paths = path.split('/');
-        if (paths.length > 0) {
-            let w = paths;
-            if (!path.endsWith('/')) {
-                w = paths.slice(0, paths.length - 1);
-                next = paths[paths.length - 1];
-            }
-            for (let i = 0, len = w.length; i < len; i++) {
-                if (isBlank(w[i]) && i > 0 && i < len)
-                    throw new Error(`Path ${path} is invalid.`);
-                await execute(callback => this.client.cwd(w[i], callback));
-                br++;
-            }
-        }
-        const download = async (name: string, parent: string, override?: string) => {
-            const elements: Client.ListingElement[] | undefined = await execute(this.client.list);
-            if (elements != null) {
-                const dirs: string[] = [];
-                for (const element of elements) {
-                    if (isBlank(name) || element.name === name) {
-                        if (element?.type === 'd') {
-                            dirs.push(element.name);
-                        } else if (element?.type === '-') {
-                            await execute(callback =>
-                                this.client.get(element.name, (error, stream) => {
-                                    if (stream != null) {
-                                        let dest = isBlank(name) || isBlank(override) ? element.name : override!;
-                                        dest = isBlank(parent) ? dest : `${parent}/${dest}`;
-                                        stream.pipe(fs.createWriteStream(dest));
-                                    }
-                                    callback(error);
-                                }));
-                        }
+    private _join(dir: string, name: string): string {
+        return isBlank(dir) ? name : `${dir}/${name}`;
+    }
+
+    private async _download(work: string, name: string, dest: string, override?: string) {
+        const elements: Client.ListingElement[] | undefined = await execute(callback => this.client.list(callback));
+        if (elements != null) {
+            const dirs: string[] = [];
+            for (const element of elements) {
+                if (isBlank(name) || element.name === name) {
+                    if (element?.type === 'd') {
+                        dirs.push(element.name);
+                    } else if (element?.type === '-') {
+                        const p = await execute(callback => this.client
+                            .get(element.name, (error, stream) => {
+                                let d: string | null = null;
+                                if (stream != null) {
+                                    d = this._join(dest, isBlank(name) || isBlank(override) ? element.name : override!);
+                                    stream.pipe(fs.createWriteStream(d));
+                                }
+                                callback(error, d);
+                            }));
+                        console.log(`Downloaded file ${this._join(work, element.name)} to ${p}`);
                     }
                 }
-                if (isNotBlank(name) && dirs.length === 0)
-                    throw new Error(`Directory or file ${name} does not exist.`)
-                for (const dir of dirs) {
-                    await execute(callback => this.client.cwd(dir, callback));
-                    const directory = isBlank(parent) ? dir : `${parent}/${dir}`;
-                    if (fs.existsSync(directory)) {
-                        if (!fs.lstatSync(path).isDirectory())
-                            throw new Error(`Path ${directory} is not a directory.`);
-                    } else {
-                        fs.mkdirSync(directory);
-                    }
-                    await download('', directory);
-                    await execute(callback => this.client.cdup(callback));
-                }
             }
-        };
-        await download(next, work, over);
-        while (br > 0) {
-            await execute(callback => this.client.cdup(callback));
-            br--;
+            if (isNotBlank(name) && dirs.length === 0)
+                throw new Error(`Directory or file ${name} does not exist.`)
+            for (const dir of dirs) {
+                await execute(callback => this.client.cwd(dir, callback));
+                const directory = this._join(dest, isBlank(name) || isBlank(override) ? dir : override!);
+                if (fs.existsSync(directory)) {
+                    if (!fs.lstatSync(directory).isDirectory())
+                        throw new Error(`Path ${directory} is not a directory.`);
+                } else {
+                    fs.mkdirSync(directory);
+                    console.log(`Created directory file ${directory}`);
+                }
+                await this._download(this._join(work, dir), '', directory);
+                await execute(callback => this.client.cdup(callback));
+            }
         }
     }
 
-    private async _cd(path: string | string[]): Promise<number> {
-        let back = 0;
+    private async _root(): Promise<string> {
+        const current: string | undefined = await execute(callback => this.client.pwd(callback));
+        if (current) {
+            const len = current.split('/').length;
+            for (let i = 1; i < len; i++) {
+                await execute(callback => this.client.cdup(callback));
+            }
+        }
+        return current as string;
+    }
+
+    private async _back(path: string) {
+        const paths = path.split('/');
+        for (let i = 0, len = paths.length; i < len; i++) {
+            if (isBlank(paths[i])) {
+                if (i === 0)
+                    await this._root();
+                else
+                    break;
+            } else {
+                await execute(callback => this.client.cwd(paths[i], callback));
+            }
+        }
+    }
+
+    private _dirname(path: string) {
+        if (isBlank(path) || path === '.')
+            return '';
+        if (path.endsWith('/'))
+            return path;
+        const paths = path.split('/');
+        if (paths.length > 0)
+            return paths.slice(0, paths.length - 1).join('/');
+        return '';
+    }
+
+    private _basename(path: string) {
+        if (isBlank(path) || path.endsWith('/') || path === '.')
+            return '';
+        const paths = path.split('/');
+        if (paths.length > 0)
+            return paths[paths.length - 1];
+        return '';
+    }
+
+    async get(path: string, dest?: string) {
+        let br = 0, work = '', next = '', over = '';
+        if (dest != null) {
+            if (dest.endsWith('/')) {
+                dest = dest.slice(0, dest.length - 1);
+            } else {
+                over = this._basename(dest);
+                dest = this._dirname(dest);
+            }
+        } else {
+            dest = '.';
+        }
+        const paths = path.split('/');
+        let back: string | undefined = undefined;
+        if (paths.length > 0) {
+            let w = paths, d = [];
+            if (!path.endsWith('/')) {
+                w = paths.slice(0, paths.length - 1);
+                next = paths[paths.length - 1];
+                if (next === '.') next = '';
+            }
+            for (let i = 0, len = w.length; i < len; i++) {
+                if (isBlank(w[i])) {
+                    if (i > 0)
+                        throw new Error(`Path ${path} is invalid.`);
+                    else
+                        back = await this._root();
+                } else if (w[i] === '.') {
+                    if (i == 0)
+                        continue;
+                    else
+                        throw new Error(`Path ${path} is invalid.`);
+                } else {
+                    await execute(callback => this.client.cwd(w[i], callback));
+                    d.push(w[i]);
+                    br++;
+                }
+            }
+            work = d.join('/');
+        }
+        await this._download(work, next, dest, over);
+        if (back != null) {
+            await this._back(back);
+        } else {
+            while (br > 0) {
+                await execute(callback => this.client.cdup(callback));
+                br--;
+            }
+        }
+    }
+
+    private async _cd(path: string | string[]): Promise<number | string> {
+        let back = null, n = 0;
         let paths: string[] = [];
         if (typeof path === 'string')
             paths = path!.split('/');
@@ -202,94 +274,91 @@ export class FtpService {
         }
         for (let i = 0, len = paths.length; i < len; i++) {
             const p = paths[i];
-            if (isBlank(p) && i > 0 && i < len - 1)
-                throw new Error(`Path ${path} is invalid.`);
-            const elements: any[] = await execute(this.client.list) ?? [];
-            const type = elements.find(value => value.name === p)?.type;
-            if (type == null)
-                await execute(callback => this.client.mkdir(p, callback));
-            if (type == null || type === 'd')
-                await execute(callback => this.client.cwd(p, callback));
-            else
-                throw new Error(`Path ${path} is not a directory.`);
-            back++;
+            if (isBlank(p)) {
+                if (i > 0 && i < len - 1)
+                    throw new Error(`Path ${path} is invalid.`);
+                else
+                    back = await this._root();
+            } else if (p === '.') {
+                if (i == 0)
+                    continue;
+                else
+                    throw new Error(`Path ${path} is invalid.`);
+            } else {
+                const elements: any[] = await execute(callback => this.client.list(callback)) ?? [];
+                const type = elements.find(value => value.name === p)?.type;
+                if (type == null)
+                    await execute(callback => this.client.mkdir(p, callback));
+                if (type == null || type === 'd')
+                    await execute(callback => this.client.cwd(p, callback));
+                else
+                    throw new Error(`Path ${path} is not a directory.`);
+                n++;
+            }
         }
-        return back;
+        return back != null ? back : n;
+    }
+
+    private async _transfer(append: boolean, path: string, work: string, name: string) {
+        await execute(callback => {
+            if (append)
+                this.client.append(path, name, callback);
+            else
+                this.client.put(path, name, callback);
+        });
+        console.log(`Transferred file ${path} to ${this._join(work, name)}`);
+    }
+
+    private async _upload(append: boolean, path: string, work: string, name?: string) {
+        const lstat = fs.lstatSync(path);
+        if (lstat.isFile()) {
+            await this._transfer(append, path, work, name ?? this._basename(path));
+        } else if (lstat.isDirectory()) {
+            if (isNotBlank(name)) {
+                await execute(callback => this.client.mkdir(name!, callback));
+                work = this._join(work, name!);
+                console.log(`Created directory ${work}`);
+                await execute(callback => this.client.cwd(name!, callback));
+            }
+            const elements = fs.readdirSync(path);
+            for (const element of elements) {
+                const p = join(path, element)
+                if (fs.lstatSync(p).isDirectory()) {
+                    await this._upload(append, p, work, element);
+                } else {
+                    await this._transfer(append, p, work, element);
+                }
+            }
+            if (isNotBlank(name)) {
+                await execute(callback => this.client.cdup(callback));
+            }
+        } else {
+            throw new Error(`Path ${path} is invalid.`);
+        }
     }
 
     private async _put(append: boolean, path: string, dest?: string) {
-        const isDir = fs.lstatSync(path).isDirectory();
-        let br = 0, dir = '', work = '', next = '';
+        let br: string | number = 0, work = '', next = '';
         if (isBlank(dest)) {
-            dir = '';
-        } else {
-            if (dest?.endsWith('/')) {
-                dir = dest?.slice(0, dest?.length - 1);
-            } else {
-                dir = dest!;
-            }
-        }
-        if (isNotBlank(dir)) {
-            if (isDir) {
-                work = dir!;
-                next = path.endsWith('/') || !dest?.endsWith('/') ? '' : basename(path);
-            } else {
-                if (dest?.endsWith('/')) {
-                    work = dir!;
-                    next = basename(path);
-                } else {
-                    work = dirname(dir!);
-                    next = basename(dir!);
-                }
-            }
-            await this._cd(work);
-        } else {
             work = '';
-            next = path.endsWith('/') ? '' : basename(path);
-        }
-        if (isDir) {
-            const upload = async (name: string, parent: string, work: string) => {
-                if (isNotBlank(name)) {
-                    await execute(callback => this.client.mkdir(name, callback));
-                    work = isNotBlank(work) ? work + '/' + name : name;
-                    core.info(`Created directory /${work}`);
-                    await execute(callback => this.client.cwd(name, callback));
-                }
-                const elements = fs.readdirSync(parent);
-                for (const element of elements) {
-                    const p = join(parent, element)
-                    if (fs.lstatSync(p).isDirectory()) {
-                        await upload(element, p, work);
-                    } else {
-                        await execute(callback => {
-                            if (append)
-                                this.client.append(p, element, callback);
-                            else
-                                this.client.put(p, element, callback);
-                        });
-                        core.info(`Transferred file ${p} to ${isNotBlank(work) ? work + '/' + element : element}`);
-                    }
-                }
-                if (isNotBlank(name)) {
-                    await execute(callback => this.client.cdup(callback));
-                }
-            };
-            await upload(next, path, work);
+            next = path.endsWith('/') ? '' : this._basename(path);
+        } else if (dest?.endsWith('/')) {
+            work = dest!;
+            next = path.endsWith('/') ? '' : this._basename(path);
         } else {
-            if (isBlank(next)) {
-                next = basename(path);
-            }
-            await execute(callback => {
-                if (append)
-                    this.client.append(path, next, callback);
-                else
-                    this.client.put(path, next, callback);
-            });
-            core.info(`Transferred file ${path} to ${isNotBlank(work) ? `${work}/${next}` : next}`);
+            work = this._dirname(dest!);
+            next = this._basename(dest!);
         }
-        while (br > 0) {
-            await execute(callback => this.client.cdup(callback));
-            br--;
+        if (isNotBlank(work))
+            br = await this._cd(work);
+        await this._upload(append, path, work, next);
+        if (typeof br === 'string') {
+            await this._back(br);
+        } else {
+            while (br > 0) {
+                await execute(callback => this.client.cdup(callback));
+                br--;
+            }
         }
     }
 
@@ -303,12 +372,12 @@ export class FtpService {
 
     async rename(oldPath: string, newPath: string) {
         await execute(callback => this.client.rename(oldPath, newPath, callback));
-        core.info(`Renamed ${oldPath} to ${newPath}`);
+        console.log(`Renamed ${oldPath} to ${newPath}`);
     }
 
     async delete(path: string) {
         await execute(callback => this.client.delete(path, callback));
-        core.info(`Deleted ${path}`);
+        console.log(`Deleted ${path}`);
     }
 
     async cd(path: string) {
@@ -321,7 +390,7 @@ export class FtpService {
             else
                 await execute(callback => this.client.cwd(path, callback));
         }
-        core.info(`Changed working directory to ${path}`);
+        console.log(`Changed working directory to ${path}`);
     }
 
     async mkdir(path: string) {
@@ -331,11 +400,11 @@ export class FtpService {
                 break;
             await execute(callback => this.client.mkdir(p, true, callback));
         }
-        core.info(`Created directory to ${path}`);
+        console.log(`Created directory to ${path}`);
     }
 
     async rmdir(path: string) {
         await execute(callback => this.client.rmdir(path, true, callback));
-        core.info(`Removed directory to ${path}`);
+        console.log(`Removed directory to ${path}`);
     }
 }
